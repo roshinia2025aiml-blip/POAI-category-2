814a2ae3942ef9e4a179b01f096c63db5463ea2e
roshinia2025aiml-blip <roshini.a.2025.aiml@rajalakshmi.edu.in>
2026-07-31 20:43:00 +0530

    Static Environment.py
    The environment does not change while the agent is working.
    Example
    Maze Solving Robot

---
+++ Static Environment.py
@@ -0,0 +1,12 @@
+maze = [
+    ["S", ".", "."],
+    ["#", "#", "."],
+    [".", ".", "G"]
+]
+
+print("=== STATIC MAZE ===")
+
+for row in maze:
+    print(row)
+
+print("\nMaze remains unchanged during execution.")
